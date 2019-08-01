4bd950cc6bca4802193a489a144d08be07807946
sunfei <sun.fei@immomo.com>
2019-08-01 14:49:52 +0800

    [update] update recordi sdk 1. 解决立即调用startRecording导致progress返回NaN的问题 2. 解决加配乐后调用startRecording，因为某些原因导致Boudary回调没有调用无法启动startRecording的问题

--- Frameworks/RecordSDK.framework/Headers/MDRecordManager.h
+++ Frameworks/RecordSDK.framework/Headers/MDRecordManager.h
@@ -7,6 +7,12 @@
 
 #import <Foundation/Foundation.h>
 
+#ifdef Debug
+#   define MDRDebugLog(fmt, ...) NSLog((@"[RecordSDK] %s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#   define MDRDebugLog(...) do {} while (0)
+#endif
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MDRecordManager : NSObject
